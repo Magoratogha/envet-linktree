0c363ee2226d3a025fe70ab4b8aa25ba34551576
Santiago Gómez <sgomez-ecw@cricut.com>
2026-05-01 15:35:41 -0500

    Add Firebase configuration and setup for hosting

---
+++ firebase-config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: '#{FIREBASE_API_KEY}#',
+  authDomain: "envet-en-casa.firebaseapp.com",
+  projectId: "envet-en-casa",
+  storageBucket: "envet-en-casa.firebasestorage.app",
+  messagingSenderId: "1001542526541",
+  appId: "1:1001542526541:web:c8399c3220671c2da19e04",
+  measurementId: "G-ED0XEJ9P61"
+};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection,} from '@angular/core';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {FIREBASE_CONFIG} from '../../firebase-config';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideFirebaseApp(() => initializeApp(FIREBASE_CONFIG)),
+    provideFirestore(() => getFirestore())
   ],
 };
